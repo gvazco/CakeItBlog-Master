f58688217af325e165850dc37245722ad5e27a74
Gustavo <gustavovazco@gmail.com>
2021-12-14 18:48:58 -0600

    margin logos

--- src/components/LogosArea/LogosArea.styles.js
+++ src/components/LogosArea/LogosArea.styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 export const Wrapper = styled.div`
   max-width: 1180px;
-  margin: 20px auto;
+  margin: 30px auto;
   padding: 20px;
   text-align: center;
 `
